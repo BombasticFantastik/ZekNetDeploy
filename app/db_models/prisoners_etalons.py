fb3a08405abd461acf588b248d3a73fce5787c99
aar001 <aar001@gmkzoloto.ru>
2026-07-15 23:07:05 +0700

    Добавил таблицу unit и все изменения связанные с ней для отметки отрядов

--- app/db_models/prisoners_etalons.py
+++ app/db_models/prisoners_etalons.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy import String, Integer, DateTime, func, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from pgvector.sqlalchemy import Vector
 from typing import List
@@ -19,12 +19,37 @@ class PrisonerEtalon(Base):
         unique=True
     )
     face_embedding: Mapped[Vector] = mapped_column(Vector(512), nullable=False)
+    unit_id: Mapped[int] = mapped_column(
+        ForeignKey("units.id"),
+        nullable=False
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), 
         default=func.now()
     )
+    
 
     attendance_logs: Mapped[List["AttendanceLog"]] = relationship(
         "AttendanceLog",
         back_populates="matched_prisoner"
     )
+    unit: Mapped["Unit"] = relationship(
+        "Unit",
+        back_populates="prisoners"
+    )
+
+
+class Unit(Base):
+    __tablename__ = "units"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(256), nullable=False)
+
+    prisoners: Mapped[List["PrisonerEtalon"]] = relationship(
+        "PrisonerEtalon",
+        back_populates="unit"
+    )    
+    sessions: Mapped[list["AttendanceSession"]] = relationship(
+        "AttendanceSession",
+        back_populates="unit"
+    )                     
